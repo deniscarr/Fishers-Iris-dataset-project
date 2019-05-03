fcd5ffde22c5b41e479318496d4b8bb53f765b5e
Denis Carr <deniscarr@engineer.com>
2019-05-03 21:30:22 +0100

    Update of fishers.py

--- fishers.py
+++ fishers.py
@@ -4,11 +4,10 @@
 
 # Importing the libaries for this project: Pandas, Numpy.
 # Pandas is an open source, BSD-licensed library providing high-performance, easy-to-use data structures and data analysis tools.
-# NumPy is the fundamental package for scientific computing with Python
- 
-
+# Numpy is a package for scientific computing with Python
 import numpy as np
 import pandas as pd
+import seaborn as sb
 
 
 def import_csv():
@@ -29,14 +28,12 @@ def show_summary_stats():
 
 	iris_data = import_csv()
 
-	# # Discovering the Shape of the table
+	#Discovering the Shape of the table
 	iris_data.shape
 
-	# # Find out unique classification/type of iris flower and the amount
+	#Find out unique classification/type of iris flower and the amount
 	iris_data['species'].unique()
 	print(iris_data.groupby('species').size())
-
-	# # Investigating the data: Min, Max, Mean, Median and Standard Deviation
 
 	# Get the minimum value of all the column in python pandas
 	iris_data.min()
@@ -53,12 +50,18 @@ def show_summary_stats():
 	# Get the standard deviation value of all the column in python pandas
 	iris_data.std()
 
-	# # Presenting the Summary Statistics a more readable way
+	#Presenting the Summary Statistics a more readable way
 	# DataFrame.describe: Generates descriptive statistics that summarize 
 	# the central tendency, dispersion and shape of a dataset’s distribution, excluding NaN values.
-	summary = iris_data.describe()
-	summary = summary.transpose()
-	print(summary)
+	iris_summary = iris_data.describe()
+	iris_summary = iris_summary.transpose()
+	print(iris_summary)
+
+
+
+def show_boxplots(): 
+	""" Compare the distributions of sepal length, sepal width, petal length and petal width using boxplots """
+	print ("Method here")
 
 
 
@@ -67,8 +70,9 @@ def display_menu():
 	""" Display menu of choices and prompt user for input choice """
 	print ("\n\n", 22 * "-" , "MENU" , 22 * "-")
 	print ("1. Show summary statistics of the iris data set (including min, max, mean, median and std)")
-	print ("2. Exit")
-	print ("Please enter your choice [1-2]: ")
+	print ("2. Compare the distributions of sepal length, sepal width, petal length and petal width using boxplots")
+	print ("3. Exit")
+	print ("Please enter your choice [1-3]: ")
 	choice = input()
 	return choice
 	
@@ -83,6 +87,8 @@ if __name__ == "__main__":
 		if choice == "1":     
 			show_summary_stats()
 		elif choice == "2":
+			show_boxplots()
+		elif choice == "3":
 			exit()
 		else:
         	# Any integer inputs other than values 1-5 - print an error message
